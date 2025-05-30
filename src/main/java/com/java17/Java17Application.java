3c746615f89067e56286bf4b2c9ef655568407ef
KEVAL CHHEDA <kvc@somaiya.edu>
2025-05-30 18:23:01 +0530

    Record Class Added 1st Program
    Record Class Added 1st Program

--- src/main/java/com/java17/Java17Application.java
+++ src/main/java/com/java17/Java17Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Java17Application {
 
 	public static void main(String[] args) {
+		System.out.println("Hello world");
 		SpringApplication.run(Java17Application.class, args);
 	}
 
